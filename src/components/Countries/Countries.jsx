d7000133ef103cbcd702abec60d2d2be5c1a33c4
suvobd100 <makbarbd@gmail.com>
2024-12-31 01:54:48 +0600

    install daisy ui with button

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -13,11 +13,14 @@ export default function Countries() {
     },[]);
 
   return (
-    <div>
-        <h3 className="font-bold, text-4xl text-slate-700">
+    <div className="w-11/12 mx-auto flex flex-col items-center">
+        <div className="text-center font-bold, text-4xl text-slate-700">
+        <h1>World Country Name with Flag</h1>
+        <h3 className="font-bold, text-2xl text-slate-500 mt-5 mb-5">
             Countries: {countries.length}
         </h3>
-        <div >
+        </div>
+        <div className="grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4 justify-center items-center ">
                     {
                         countries.map(c =><Country key={c.ccn3} props={c}></Country>)
                     }
--- src/components/County/Country.jsx
+++ src/components/County/Country.jsx
@@ -1,23 +1,39 @@
+import { useState } from "react";
 
 export default function Country({props}) {
-    console.log(props);
-    const{name: {common} }= props;
-    
-    const { flags: { png: flagPng } } = props;
-    const {population} = props;
-    const {area} = props;
-    const {capital} = props;
-    const {timezones} = props;
+    // console.log(props);
+    const { 
+        name: { common }, 
+        flags: { png: flagPng }, 
+        population, 
+        area, 
+        capital, 
+        timezones 
+      } = props;
 
-  console.log(population); 
+
+//   console.log(population); 
+
+const [visited, setVisited] = useState(false);
+
+const handleVisited = ()=>{
+        setVisited(!visited)
+}
+
+
   return (
-    <div className="mt-3 max-w-fit border-2 border-indigo-600 rounded-xl mb-5 w-80 p-5 ">
+    <div className="mt-3 border-2 border-indigo-600 rounded-xl mb-5 lg:w-80 w-4/4 p-5 text-center">
         <span className="font-bold text-xl">Country Name : {common}</span>
-        <img className="w-2/4 object-cover rounded-md mt-5" src={flagPng} alt="" />
+        <span className="justify-center items-center flex">
+        <img className="w-32 h-32 object-contain rounded-md mt-5" src={flagPng} alt="" />
+        </span>
+        
         <p className="mt-4 font-bold">Population: {population}</p>
         <p className="mt-4 font-bold">Area: {area}</p>
         <p className="mt-4 font-bold">Capital: {capital}</p>
-        <p className="mt-4 font-bold">Time Zone: {timezones}</p>
+        <p className="mt-4 mb-5 font-bold">Time Zone: {timezones}</p>
+        <button onClick={handleVisited} className="btn btn-active">Visited</button>
+        
     </div>
   )
 }
